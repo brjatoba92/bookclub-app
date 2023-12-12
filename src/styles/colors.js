59b0d6567992cdb856e63fc8b243832e41fc2458
brjatoba92 <brjatoba92@gmail.com>
2023-12-12 18:32:37 -0300

    feat - ChakraUI implementation - colors global styles

---
+++ src/styles/colors.js
@@ -0,0 +1,10 @@
+export const colors = {
+    primary: '#FFC734',
+    black: '#000',
+    grayDark: '#737373',
+    gray: '#E6E6E6',
+    grayLight: '#F2F1F1',
+    background: '#F2F1F1',
+    white: '#fff'
+
+}
