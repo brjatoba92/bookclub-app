4e2f2a0ab6d4de4e2a4c552a7d166c4264a43061
brjatoba92 <brjatoba92@gmail.com>
2023-12-15 14:47:21 -0300

    feat: React Query Implementation to Check whether the User is registred or not on the Register Screen - User e-mail already exists

--- src/modules/unauthenticated/screens/Register/index.js
+++ src/modules/unauthenticated/screens/Register/index.js
@@ -1,4 +1,4 @@
-import { Flex, Image } from '@chakra-ui/react'
+import { Flex, Image, useToast } from '@chakra-ui/react'
 import { Text, Input, Link, Button} from 'components'
 import { useNavigate } from 'react-router-dom'
 import { useFormik } from 'formik'
@@ -8,12 +8,28 @@ import { registerCall } from 'services/api/requests'
 
 export const RegisterScreen = () => {
     const navigate = useNavigate()
+    const toast = useToast()
     const mutation = useMutation((newUser) => registerCall(newUser), {
       onError: (error) => {
-        console.log({ error })
+        // console.log({ error })
+        toast({
+          title: 'Falha ao criar a conta.',
+          description: error?.response?.data?.error || 'Por favor, tente novamente',
+          status: 'error',
+          duration: 3000,
+          isClosable: true,
+        })
+
       },
       onSuccess: (data) => {
-        console.log({ data })
+        // console.log({ data })
+        toast({
+          title: 'Account created.',
+          description: "We've created your account for you.",
+          status: 'success',
+          duration: 6000,
+          isClosable: true,
+        })
       }
     })
 
