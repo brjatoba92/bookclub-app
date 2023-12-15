6174e70dd95b5eea4511453997a007028652b5bb
brjatoba92 <brjatoba92@gmail.com>
2023-12-14 21:17:58 -0300

    feat: Form Validation Implementation with Formik and Yup - Add Input Errors Messages in Register Screen

--- src/modules/unauthenticated/screens/Register/index.js
+++ src/modules/unauthenticated/screens/Register/index.js
@@ -1,9 +1,34 @@
 import { Flex, Image } from '@chakra-ui/react'
 import { Text, Input, Link, Button} from 'components'
 import { useNavigate } from 'react-router-dom'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 export const RegisterScreen = () => {
     const navigate = useNavigate()
+
+    const { handleSubmit, values, handleChange, errors} = useFormik({
+      initialValues: {
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+      },
+      validationSchema: Yup.object({
+        name: Yup.string().min(3, 'Nome deve ter ao menos 3 caracteres').required('Nome é obrigatório'),
+        email: Yup.string().email('E-mail invalido').required('E-mail é obrigatorio.'),
+        password: Yup.string().min(6, 'Senha deve ter ao menos 6 caracteres').required('Senha é obrigatorio.'),
+        confirmPassword: Yup.string()
+          .min(6, 'Conformar a senha deve ter ao menos 6 caracteres')
+          .required('Conformar a senha é obrigatorio.')
+          .oneOf([Yup.ref('password'), null], 'Senhas não são iguais')
+      }),
+      onSubmit: (data) => {
+        console.log({ data })
+      }
+    })
+    // console.log({ values, errors })
+
     return(
         <Flex w='100vw' h='100vh' flexDir='row'>
             <Flex
@@ -26,11 +51,45 @@ export const RegisterScreen = () => {
                   flexDir='column'
                 >
                   <Text.ScreenTitle marginTop='48px'>Registre</Text.ScreenTitle>
-                  <Input mt='24px' placeholder="Your Name"/>
-                  <Input mt='16px' placeholder="E-mail"/>
-                  <Input.Password mt='16px' placeholder="Password"/>
-                  <Input.Password mt='16px' placeholder="Confirm Password"/>
-                  <Button mt='24px' mb='12px' >Register</Button>
+                  <Input
+                    type='text'
+                    id='name'
+                    name='name'
+                    value={values.name}
+                    mt='24px'
+                    placeholder="Your Name"
+                    onChange = {handleChange}
+                    error={errors.name}
+                  />
+                  <Input
+                    type='email'
+                    id='email'
+                    name='email'
+                    value={values.email}
+                    mt='16px'
+                    placeholder="E-mail"
+                    onChange = {handleChange}
+                    error={errors.email}
+                  />
+                  <Input.Password
+                    id='password'
+                    name='password'
+                    value={values.password}
+                    mt='16px'
+                    placeholder="Password"
+                    onChange = {handleChange}
+                    error={errors.password}
+                  />
+                  <Input.Password
+                    id='confirmPassword'
+                    name='confirmPassword'
+                    value={values.confirmPassword}
+                    mt='16px'
+                    placeholder="Confirm Password"
+                    onChange = {handleChange}
+                    error={errors.confirmPassword}
+                  />
+                  <Button mt='24px' mb='12px' onClick={handleSubmit} >Register</Button>
                   <Link.Action
                     onClick = {() => navigate('/')}
                     mt='8px'
