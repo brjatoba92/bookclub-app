0480b21cafa1712a92374443bc9e77bc0be3ba3a
brjatoba92 <brjatoba92@gmail.com>
2023-12-22 22:26:58 -0300

    fix: Category List  implementation in Home Screen with api.get and LocalStorage - Refactor Selected Category

--- src/components/organisms/BookList.js
+++ src/components/organisms/BookList.js
@@ -1,13 +1,20 @@
 import { Flex } from '@chakra-ui/react'
 import { BookCard } from 'components/molecules'
 import { Text } from 'components/atoms'
+import { useQuery } from 'react-query'
+import { getHightlightedBooks } from 'services/api/requests'
 
-export const BookList = ({data, error, isLoading}) => {
+export const BookList = () => {
+    const { data } = useQuery(
+        'highlighted',
+        getHightlightedBooks
+    )
     return (
         <Flex mt='48px' paddingX={['24px', '48px', '80px', '112px']} flexDir='column'>
             <Text.ScreenTitle mb='20px' >Destaques</Text.ScreenTitle>
             <Flex flexDir='row'>
-                {data && data?.map((item) => <BookCard key={`book_${item.id}`} {...item} />)}
+                {data?.data &&
+                  data?.data.map((item) => <BookCard key={`book_${item.id}`} {...item} />)}
             </Flex>
         </Flex>
     )
