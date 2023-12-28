842fb2ef71ab04546c2d8d31ad2cf767ff40dd1d
brjatoba92 <brjatoba92@gmail.com>
2023-12-28 20:32:18 -0300

    feat: Search Screen implementation - search result when digited and show books

--- src/components/organisms/BookList.js
+++ src/components/organisms/BookList.js
@@ -1,34 +1,36 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, Spinner } from '@chakra-ui/react'
 import { BookCard } from 'components/molecules'
 import { Text } from 'components/atoms'
-import { useQuery } from 'react-query'
-import { getHightlightedBooks } from 'services/api/requests'
+// import { useQuery } from 'react-query'
+// import { getHightlightedBooks } from 'services/api/requests'
 
-export const BookList = () => {
-    const { data } = useQuery(
-        'highlighted',
-        getHightlightedBooks
-    )
+export const BookList = ({ title, data, isLoading }) => {
+    // const { data } = useQuery('highlighted', getHightlightedBooks)
     return (
         <Flex
           mt={['24px','48px']}
           paddingX={['24px', '48px', '80px', '112px']}
           flexDir='column'
-          
+          overflowX={['scroll', 'auto']}
+          css={{
+            '::-webkit-scrollbar': {
+              display: 'none'
+            }
+          }}
         >
-            <Text.ScreenTitle >Destaques</Text.ScreenTitle>
-            <Flex
-              flexDir='row'
-              mt={['12px', '24px']}
-              overflowX={['scroll', 'auto']}
-              css={{
-                '::-webkit-scrollbar': {
-                    display: 'none'
-                }
-              }}
-            >
-                {data?.data &&
-                  data?.data.map((item) => <BookCard key={`book_${item.id}`} {...item} />)}
+            <Text.ScreenTitle >{title}</Text.ScreenTitle>
+            <Flex flexDir='row' mt={['12px', '24px']}>
+              {
+                isLoading && (
+                  <Flex alignItems='center' justifyContent='center' h='230px'>
+                    <Spinner />
+                  </Flex>
+                )
+              }
+              {data &&
+                  data?.map((item) => (
+                    <BookCard key={`book_${item.id}`} {...item} />
+                  ))}
             </Flex>
         </Flex>
     )
